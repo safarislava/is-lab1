b121874424b2a9e342bb6d20605a5770f4d259c0
safarislava <safarislava@gmail.com>
2026-09-22 13:44:39 +0300

    refactor: add serial verison uid

--- backend/src/main/java/ru/ifmo/se/application/dto/response/BookCreatureNotFoundException.java
+++ backend/src/main/java/ru/ifmo/se/application/dto/response/BookCreatureNotFoundException.java
@@ -1,6 +1,11 @@
 package ru.ifmo.se.application.dto.response;
 
+import java.io.Serial;
+
 public class BookCreatureNotFoundException extends RuntimeException {
+    @Serial
+    private static final long serialVersionUID = 1L;
+
     public BookCreatureNotFoundException(int creatureId) {
         super(String.format("Creature not found: %d", creatureId));
     }
--- backend/src/main/java/ru/ifmo/se/application/dto/response/RingAlreadyInUseException.java
+++ backend/src/main/java/ru/ifmo/se/application/dto/response/RingAlreadyInUseException.java
@@ -2,8 +2,13 @@ package ru.ifmo.se.application.dto.response;
 
 import ru.ifmo.se.persistence.entity.BookCreature;
 
+import java.io.Serial;
+
 public class RingAlreadyInUseException extends RuntimeException {
-    public RingAlreadyInUseException(Integer creatureId) {
-        super(String.format("Ring is already in use by creature: %d", creatureId));
+    @Serial
+    private static final long serialVersionUID = 1L;
+
+    public RingAlreadyInUseException(BookCreature bookCreature) {
+        super(String.format("Ring is already in use by creature: %s", bookCreature.getName()));
     }
 }
