1a2827f248e4084cf5d85abd3fa3348c52f90919
safarislava <safarislava@gmail.com>
2026-09-22 13:45:01 +0300

    feat: add entity manager producer

---
+++ backend/src/main/java/ru/ifmo/se/persistence/config/EntityManagerProducer.java
@@ -0,0 +1,26 @@
+package ru.ifmo.se.persistence.config;
+
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.enterprise.context.RequestScoped;
+import jakarta.enterprise.inject.Disposes;
+import jakarta.enterprise.inject.Produces;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
+
+@ApplicationScoped
+public class EntityManagerProducer {
+    private final EntityManagerFactory emf = Persistence.createEntityManagerFactory("is-lab1-pu");
+
+    @Produces
+    @RequestScoped
+    public EntityManager createEntityManager() {
+        return emf.createEntityManager();
+    }
+
+    public void closeEntityManager(@Disposes EntityManager em) {
+        if (em.isOpen()) {
+            em.close();
+        }
+    }
+}
